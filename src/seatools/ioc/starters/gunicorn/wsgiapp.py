406d091a554cae6d061693e6ad2d3f2283dc2a9c
dragons96 <jinqunlong@black-unique.com>
2024-12-20 18:43:48 +0800

    init

--- src/seatools/ioc/starters/gunicorn/wsgiapp.py
+++ src/seatools/ioc/starters/gunicorn/wsgiapp.py
@@ -1,29 +1,38 @@
 import importlib
 
-from gunicorn.app.wsgiapp import WSGIApplication as GunicornWSGIApplication
+from gunicorn.app.wsgiapp import Application
 from gunicorn.config import Setting, validate_string
 from seatools.ioc.config import cfg
 
 
-class WSGIApplication(GunicornWSGIApplication):
+class WSGIApplication(Application):
+    """seatools.ioc WSGIApplication."""
 
     def init(self, parser, opts, args):
-        super().init(parser, opts, args)
         if len(args) > 0:
             # ioc app run
             ioc_app_path = args[0]
             module_name, func_name = ioc_app_path.split(':')
             self.ioc_app = getattr(importlib.import_module(module_name), func_name)
             self.ioc_app()
-            app =  args[1] if len(args)>1 else  (((cfg().get('seatools') or {}).get('server') or {}).get('gunicorn') or {}).get('app', 'seatools.ioc.server.app:wsgi_app')
-            print("gunicorn app:", app)
-            self.cfg.set("default_proc_name", app)
-            self.app_uri = app
-
+            config = (((cfg().get('seatools') or {}).get('server') or {}).get('gunicorn') or {})
+            if not isinstance(config, dict):
+                config = {}
+            self.app_factory_path = config.get('app', 'seatools.ioc.server.app:wsgi_app')
+            if len(args) > 1:
+                self.app_factory_path = args[1]
+            # server config override
+            for k, v in config.items():
+                if k != 'app':
+                    self.cfg.set(k, v)
+    
+    def load_wsgiapp(self):
+        module_name, func_name = self.app_factory_path.split(":")
+        return getattr(importlib.import_module(module_name), func_name)()
+                
     def load(self):
-        if hasattr(self, 'ioc_app'):
-            self.ioc_app()
-        super().load()
+        self.ioc_app()
+        return self.load_wsgiapp()
 
 
 class IocAppSetting(Setting):
