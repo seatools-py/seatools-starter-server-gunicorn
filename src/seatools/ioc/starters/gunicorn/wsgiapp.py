61de6736750af7cb73bbb3468b15458c36d9c869
dragons96 <jinqunlong@black-unique.com>
2024-12-20 17:25:00 +0800

    init

--- src/seatools/ioc/starters/gunicorn/wsgiapp.py
+++ src/seatools/ioc/starters/gunicorn/wsgiapp.py
@@ -15,7 +15,8 @@ class WSGIApplication(GunicornWSGIApplication):
             module_name, func_name = ioc_app_path.split(':')
             self.ioc_app = getattr(importlib.import_module(module_name), func_name)
             self.ioc_app()
-            app =  args[1] if len(args)>1 else  (((cfg().get('seatools') or {}).get('server') or {}).get('gunicorn') or {}).get('app', 'seatools.ioc.server.app:asgi_app')
+            app =  args[1] if len(args)>1 else  (((cfg().get('seatools') or {}).get('server') or {}).get('gunicorn') or {}).get('app', 'seatools.ioc.server.app:wsgi_app')
+            print("gunicorn app:", app)
             self.cfg.set("default_proc_name", app)
             self.app_uri = app
 
